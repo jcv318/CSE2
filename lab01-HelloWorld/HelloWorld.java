d89dfb6aa4aadf57975ce48ab4b750af43c4abe5
jcv318 <jcv318@lehigh.edu>
2015-08-28 15:46:28 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//Jackson Viscomi
+//Hello World Java Program
+
+//define a class
+public class HelloWorld{
+    
+    //add a method
+    public static void main(String[] args) {
+        
+        //printing out statement
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#  Hello World 2015!! #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        
+    }
+}
