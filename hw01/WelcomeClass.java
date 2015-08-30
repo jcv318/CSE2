25f88103274d6ac1b71e0063edb1b126210a74a3
jcv318 <jcv318@lehigh.edu>
2015-08-30 18:02:02 +0000

    updated Welcome Classs

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Jackson Viscomi
+//Welcome Class hw01
+
+//define a class
+public class WelcomeClass{
+    
+    //add a method
+    public static void main(String[] args) {
+        
+        //printing out statement
+        System.out.println("    -------------      ");
+        System.out.println("    |   WELCOME  |     ");
+        System.out.println("    -------------      ");
+        System.out.println("    ^  ^  ^  ^  ^  ^   ");
+        System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println("  <-J--C--V--3--1--8-> ");
+        System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /  ");
+        System.out.println("    v  v  v  v  v  v   ");
+        System.out.println(" Hi my name is Jackson and I ran a 100K trail race this summer");
+        
+        
+        
+    }
+}
