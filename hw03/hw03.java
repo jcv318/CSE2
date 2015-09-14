0d5c67cb93e7427907ef7e05951c553d579d7afa
jcv318 <jcv318@lehigh.edu>
2015-09-14 21:36:06 +0000

    updated hw03

--- hw03/hw03.java
+++ hw03/hw03.java
@@ -6,27 +6,18 @@ public class hw03{
 
 Scanner myScanner = new Scanner ( System.in ) ; //construct my own scanner
 
-System.out.print("Enter the current time");
-int currentTime = myScanner.nextInt();
+System.out.print("Enter the current time in military HHMM form"); //prompts user to enter time 1
+int currentTime = myScanner.nextInt(); //tells code to use value as current time
 
-System.out.print("Enter the time that you will be eating dinner");
-int dinnerTime = myScanner.nextInt();
+System.out.print("Enter the time that you will be eating dinner"); //prompts user to enter time 2
+int dinnerTime = myScanner.nextInt(); //tells code to use value as dinner time
 
-double difference = dinnerTime - currentTime; 
+double difference = dinnerTime - currentTime; //takes differnce of the two times
 
-int hours =(int)difference ;
-hours= (hours/100);
-double mins = difference % 100; 
-System.out.println("You have " +hours+ " hours and " + mins + " minutes until dinner");
+int hours =(int)difference ; //converts differnce into integer for decimal purposes
+hours= (hours/100); //now that hours is in single decimal int, divding it by 100 wil result in the number of hours
+double mins = difference % 100; // minutes is calculated by taking the remainder when dividing the differnce by 100
+System.out.println("You have " +hours+ " hours and " + mins + " minutes until dinner"); //displays the result
 
-
-
-
-   			    
-   			    
-   			    
-   			    
-   			    
-   			    
    			}
 }
