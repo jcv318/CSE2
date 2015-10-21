3bc12d26a9098da89c08b01f6254c3266bbff022
jcv318 <jcv318@lehigh.edu>
2015-10-21 03:18:48 +0000

    updated hw08 with comments

--- hw08/Area.java
+++ hw08/Area.java
@@ -1,52 +1,52 @@
-import java.util.Scanner;
+import java.util.Scanner; 
 public class Area{
 public static void main (String [] agrs){
 Scanner myScanner = new Scanner (System.in);
 boolean goodInput = false; //intially declares "goodimput
 
-String tri = "triangle";
+String tri = "triangle"; //initializing shapes 
 String rec = "rectangle";
 String cir = "circle";
 String shape;
 double area=0;
 
 
-do{
+do{ //boolean do while input to make sure input is correct
     goodInput = false;
     System.out.print("enter a shape: ");
     shape = myScanner.nextLine();
         if (shape.equals(tri)||shape.equals(rec)||shape.equals(cir)){
-           goodInput=true;
+           goodInput=true; //if the input matches, boolean is cool 
         }
         else{
             System.out.println("enter a valid shape");
-            goodInput=false;
+            goodInput=false; //else, keep asking
         }
-    }while(!goodInput);
+    }while(!goodInput); //only ends with true boolean 
 
-double height=0;
+double height=0; //initialzing all parameters
 double base=0;
 double width=0;
 double length=0;
 double radius=0;
 goodInput=false;
 
-if(shape.equals(tri)){
+if(shape.equals(tri)){ //if user puts in 'triangle'
     
     do {                                                                    
         try {
         
-         System.out.print("enter base ");
+         System.out.print("enter base "); //tells user to input base
             base = myScanner.nextDouble();
          goodInput = true;
-    } catch (Exception e) {
+    } catch (Exception e) { //must be int
      System.out.println("Enter an int, please");
      goodInput =false;
      String clear = myScanner.nextLine();
     } 
     }while (!goodInput);
     
-    do {                                                                    
+    do {                                 //user enters height                                   
         try {
         goodInput = true;
          System.out.print("enter height ");
@@ -59,12 +59,12 @@ if(shape.equals(tri)){
     } 
     }while (!goodInput);
     
-area=((base*height)/2);
+area=((base*height)/2); //area formula
 System.out.println("Area of triangle is  "+area);
     
 }
     
-if(shape.equals(rec)){
+if(shape.equals(rec)){ //if user chooses rectangle
     
     do {                                                                    
         try {
@@ -96,7 +96,7 @@ area=(width*length);
 System.out.println("Area of Rectangle is  "+area);
 }
 
-if(shape.equals(cir)){
+if(shape.equals(cir)){ //if user enters circle
     
     do {                                                                    
         try {
@@ -112,10 +112,10 @@ if(shape.equals(cir)){
     }while (!goodInput);
     
     
-area=((Math.PI)*(radius*radius));
+area=((Math.PI)*(radius*radius)); //used Math.PI to get 3.14 value
 System.out.println("Area of Circle is  "+area);
 }
     
     
 }
-}
+}//end
--- hw08/StringAnalysis.java
+++ hw08/StringAnalysis.java
@@ -1,28 +1,42 @@
-import java.util.Scanner;
-public class StringAnalysis{
+/*  Jackson Viscomi
+    10/20/15
+    CSE 002
+    HW 08
+    
+This Program uses the principal of method overloading by asking the user
+to enter a string, and then ask them if they want to evaluate a certain
+number of characters or the entire string. Depending on whether they entered
+a string or they chose to evaluate, a certain method (all under the same name)
+will evaluate it, and tell the user if it is all letters or not.
+*/
+    
+    
 
-public static boolean stringCheck(String string2, int number){
-int stringLength2 = string2.length();
+import java.util.Scanner; //import scanner
+public class StringAnalysis{ // start of full program
+
+public static boolean stringCheck(String string2, int number){ //string check if user asks for certain int
+int stringLength2 = string2.length(); //counts number of characters
 int count=0;
 do{
-    char place=string2.charAt(count);
+    char place=string2.charAt(count); //makes the counter go down the characters
     if (!Character.isLetter(place)){
-        break;
+        break; //ends the loop when limit is reached
     }
 
-    count=count+1;
-}while(count<stringLength2 && count<number);
+    count=count+1; //increases count
+}while(count<stringLength2 && count<number); //only happens within bounds of desired count
 if (count>=number || count==stringLength2){
-    return true;
+    return true; //when count is reached, cycle ends
 }
 else{
-    return false;
+    return false; //if it isn't over, keep running the loop
     
 }
     
 }
 
-public static boolean stringCheck(String string2){
+public static boolean stringCheck(String string2){ //(if user imputs 'all') same as last, except minus the number counter
 int count =0;
 int stringLength2 = string2.length();
  do{
@@ -45,48 +59,48 @@ Scanner myScanner = new Scanner (System.in);
 
 
 
-boolean goodInput=false;
+boolean goodInput=false; //setting boolean 
 
 
-System.out.print("enter a string: ");
-String input = myScanner.next();
+System.out.print("enter a string: "); //prompting user for string
+String input = myScanner.next(); //input is what he types
 
 System.out.print("imput the number of characters you would like to evaluate or type 'all'");
 
 do{
-    if(myScanner.hasNextInt()){
+    if(myScanner.hasNextInt()){ //if user put in integer
         int number=myScanner.nextInt();
-        if (number>0){
-            if(stringCheck(input,number)){
-            System.out.println("All Letters");
+        if (number>0){ //number must be positive
+            if(stringCheck(input,number)){ //if method is true, they are all letters
+            System.out.println("All Letters"); 
         }
-        else{
+        else{ //if not true, there is an an int somewhere
             System.out.println("Not all letters");
         }
         goodInput=true;
     }
-        else{
+        else{ //if they put in something other than what I said, tell them to put input in again
         System.out.println("Please enter number of characters or type 'all'");
     }
     }
     
-    else if(myScanner.hasNext("all")){
-        if(stringCheck(input)){
+    else if(myScanner.hasNext("all")){ //if the person imputs 'all'
+        if(stringCheck(input)){ //if method is true, all letters
             System.out.println("All letters");
         }
-        else{
+        else{ //if not true, there is an int somwhere
             System.out.println("Not all letters");
         }
     goodInput=true;
     }
-    else{
+    else{ //if they put in a wrong input, ask them again
         System.out.println("Please enter number of characters or type 'all'");
     
         myScanner.next();
     }
-}while(!goodInput);
+}while(!goodInput); //only good until boolean is true
 }
-}
+} //end
     
 
 
